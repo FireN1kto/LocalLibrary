51bcf286bcc723c8d3f6c108d6cb0fb37e6a4dde
F1re_N1kt0 <maksimcymbal77@gmail.com>
2024-12-03 13:57:32 +0700

    Create pages login, logout,registrated and others

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
     re_path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+    re_path(r'^accounts/login/$', views.LoginView.as_view(), name='login'),
+    re_path(r'^accounts/logout/$', views.LogoutView.as_view(), name='logout'),
+    re_path(r'^accounts/register/$', views.RegisterView.as_view(), name='register'),
+    re_path(r'^accounts/register/done/$', views.RegisterDoneView.as_view(), name='register-done'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,8 @@ from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 
 def index(request):
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     num_books=Book.objects.all().count()
     num_instances=BookInstance.objects.all().count()
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
@@ -11,7 +13,9 @@ def index(request):
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_visits': num_visits},
     )
 
 class BookListView(generic.ListView):
